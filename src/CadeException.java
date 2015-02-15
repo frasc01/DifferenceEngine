7ba445b0f3e6a6381edb65c32096b9b28adb2652
frasc01 <swfraser@optonline.net>
2015-02-15 01:10:20 -0500

    Update to not use Template in Constructor

---
+++ src/CadeException.java
@@ -0,0 +1,8 @@
+
+public class CadeException extends Exception {
+	private static final long serialVersionUID = -1543931432350783056L;
+	CadeException(String msg) {
+		super(msg);
+	}
+	
+}
---
+++ src/Difference.java
@@ -0,0 +1,40 @@
+import java.util.List;
+
+/**
+ * 
+ * @author swfraser
+ *  The Difference class holds a list of the comparable objects
+ *  These lists will contain the segments which were divided up by the difference engine.
+ *  The segment Object must be a comparable object.  (Base classes will not work)
+ *  If you use a base class object, they will be converted to the Java wrapper class by the difference engine.
+ *
+ */
+public class Difference  {
+	enum PLACEMENT {A_ONLY, B_ONLY, BOTH};
+	PLACEMENT m_location; // This determines the placement of this segment.
+	List<?> m_segment;    // This is a list of the segment for this classes dataType 
+
+	Difference(PLACEMENT location, List<Object> segment){
+		m_segment = segment;
+		m_location = location;
+	}
+	
+	Difference(){
+	}
+	
+	public void setSegment(List<?> m_obj1) {
+		m_segment = m_obj1;
+	}
+
+	public List<?> getSegment() {
+		return m_segment;
+	}
+	
+	public void setLocation(PLACEMENT  location) {
+		m_location = location;
+	}
+
+	public PLACEMENT getLocation() {
+		return m_location;
+	}
+}
